e67955a7baacd043f1f71aadaf16046b1a1a2944
Deyvid <gdeyvid674@gmail.com>
2026-06-18 21:27:52 -0300

    feat: inplementa menu principal

--- Program.cs
+++ Program.cs
@@ -1,15 +1,54 @@
 ﻿Filme filme = new Filme();
 
-Console.Write("Título: ");
-filme.titulo = Console.ReadLine();
+int opcao;
 
-Console.Write("Genero: ");
-filme.genero = Console.ReadLine();
+do
+{
+    Console.WriteLine("\n===CATÁLOGO DE FILMES===");
+    Console.WriteLine("1 - Cadastrar filme");
+    Console.WriteLine("2 - Exibir filme");
+    Console.WriteLine("0 - Sair");
+    Console.Write("Escolha uma opição: ");
+    opcao = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Ano de Lançamento: ");
-filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
+    switch (opcao)
+    {
+        case 1;
+            Console.Write("Título: ");
+            filme.titulo = Console.ReadLine();
 
-Console.Write("Classificação indicativa: ");
-filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Genero: ");
+            filme.genero = Console.ReadLine();
 
-filme.ExibirInformacaos();
+            Console.Write("Ano de Lançamento: ");
+            filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Classificação indicativa: ");
+            filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nFilme cadastrado com susesso!");
+            break;
+        case 2:
+            if (string.IsNullOrEmpty(filme.titulo))
+            {
+                Console.WriteLine("\nNenhun filme cadastraado ainda. ");
+            }
+            else
+            {
+                filme.ExibirInformacaos();
+            }
+            break;
+        case 0:
+            Console.WriteLine("\nPrograma encerrado. ");
+            break;
+
+        default:
+            Console.WriteLine("\nOpição inválida. ");
+            break;
+
+    }
+
+} while (opcao != 0);
+
+
+
